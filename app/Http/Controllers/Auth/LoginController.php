f899c10f19ce6328b91472f0a04206d15493dd97
Dannie <dannie@thenextcoders.com>
2026-07-15 17:45:00 +0545

    login flow created

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class LoginController extends Controller
+{
+    public function login(Request $request){
+        //Validate Request
+        $validated = $request->validate([
+            'email' => "required|email",
+            'password' => 'required'
+        ]);
+        
+
+        //find user by email
+        $user = User::where('email',$validated['email'])->first();
+
+        //check if user is found
+        if (!$user){
+            return response()->json([
+                'message' => 'Invalid email'
+            ], 401);
+        }
+        //check is password is correct
+        if(!Hash::check($validated['password'],$user->password)){
+            return response()->json([
+                'message' => 'Invalid password'
+            ], 401);
+        }
+
+        //Generate sanctum token
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+
+
+        // 5. Return response
+        return response()->json([
+            'message' => 'Login successful',
+
+            'user' => [
+                'id' => $user->id,
+                'first_name' => $user->first_name,
+                'email' => $user->email,
+            ],
+
+            'token' => $token
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+//Auth
 Route::post('/register', [RegisterController::class, 'register']);
+Route::post('/login',[LoginController::class, 'login']);
+Route::middleware('auth:sanctum')
+    ->get('/me', function(Request $request){
+
+        return $request->user();
+
+    });
